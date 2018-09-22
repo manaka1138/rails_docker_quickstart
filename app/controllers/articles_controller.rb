63b6df1793fd0f94376a4099176ce1ddd5201bf3
mana1138 <mana.0826.ta@gmail.com>
2018-09-22 21:12:08 +0900

    5.10 検証 (バリデーション) の追加　922

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,9 +9,12 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
 
-  @article.save
-  redirect_to @article
-end
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
 
 
   def index
