50c52d291308fc696c02450edd3be5042607bfca
mana1138 <mana.0826.ta@gmail.com>
2018-09-23 21:11:22 +0900

    5.11

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,7 @@ class ArticlesController < ApplicationController
    end
 
   def new
+    @article = Article.new
   end
 
   def create
@@ -20,6 +21,22 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+
+def update
+  @article = Article.find(params[:id])
+
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
+end
 
 private
   def article_params
