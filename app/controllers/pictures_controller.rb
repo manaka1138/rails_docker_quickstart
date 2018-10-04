381ca26094a8757c4bbc9fcc34d13be0ea2d0957
mana1138 <mana.0826.ta@gmail.com>
2018-10-04 13:54:41 +0900

    create NameError ??

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,18 +1,20 @@
 class PicturesController < ApplicationController
   def index
   end
-　def show
-     @picture = Picture.find(params[:id])
+  def show
+    @picture = Picture.find(params[:id])
   end
   def new
   end
-  def create
-    @picture = Picture.new(params[:picture])
 
-　　 @picture.save
-　　　 redirect_to @picture
-   end
-   private
-     def picture_params
-       params.require(:picture).permit(:title, :text)
-     end
+def create
+  @picture = Picture.new(picture_params)
+
+  @picture.save
+  redirect_to @picture
+end
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+end
