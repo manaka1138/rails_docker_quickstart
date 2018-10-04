120d62102a59d65300a090ae91b1d9c61a5e7bf9
mana1138 <mana.0826.ta@gmail.com>
2018-10-05 01:19:37 +0900

    白紙一覧表示

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,14 +1,14 @@
 class PicturesController < ApplicationController
   def index
+   @pictures = Picture.all
   end
   def show
     @picture = Picture.find(params[:id])
   end
   def new
   end
-
-　def create
-    @picture = Picture.new(picture_params)
+  def create
+  @picture = Picture.new(picture_params)
 
     @picture.save
   redirect_to @picture
@@ -16,5 +16,5 @@ end
   private
   def picture_params
     params.require(:picture).permit(:title, :text)
-  end　
+  end
 end
