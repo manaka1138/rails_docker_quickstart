48a6e45635350bf4f942338411a6600620ce42b8
mana1138 <mana.0826.ta@gmail.com>
2018-10-08 23:33:23 +0900

    まだ不親切

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,17 +2,25 @@ class PicturesController < ApplicationController
   def index
    @pictures = Picture.all
   end
+
   def show
     @picture = Picture.find(params[:id])
   end
+
   def new
+    @picture = Picture.new
   end
+
   def create
   @picture = Picture.new(picture_params)
 
-    @picture.save
+    if @picture.save
   redirect_to @picture
-end
+    else
+      render 'new'
+   end
+ end
+
   private
   def picture_params
     params.require(:picture).permit(:title, :text)
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,5 @@
+
 class Picture < ApplicationRecord
+  validates :title, presence: true,
+                    length: { minimum: 5 }
 end
