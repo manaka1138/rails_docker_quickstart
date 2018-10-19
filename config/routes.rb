fee7a6d2cea189f8d2c1c3d4cd1a48de324ed676
mana1138 <mana.0826.ta@gmail.com>
2018-10-19 23:29:10 +0900

    delete destroy

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'uploader/form'
   post 'uploader/upload'
   get 'uploader/download'
-  get 'uploader/destroy'
+  delete 'uploader/:id' => 'uploader#destroy', as: 'uploader_delete'
   get 'pictures/index'
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
